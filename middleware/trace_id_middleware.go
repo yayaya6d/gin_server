5980b2199e61e1d6282b52061566012a731f5627
Jeff Tang <yayaya6d16@gmail.com>
2021-06-25 00:55:19 +0800

    refine trace id middleware

--- middleware/trace_id_middleware.go
+++ middleware/trace_id_middleware.go
@@ -1,15 +1,20 @@
 package middleware
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
 
 func TraceIDMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		traceID := uuid.New()
+		if ctx.Request.Header.Get("trace_id") == "" {
+			traceID := uuid.New()
+			ctx.Request.Header.Set("trace_id", traceID.String())
+			fmt.Println("new trace id =", traceID)
+		}
 
-		ctx.Header("trace_id", traceID.String())
 		ctx.Next()
 	}
 }
